9cfad4c658b4e55fa5ce9e61b2c0f2a77f7bc185
Rob Wilds <Robert.wilds@hyland.com>
2023-07-24 10:41:19 -0400

    updated proxy.conf

--- proxy.conf.js
+++ proxy.conf.js
@@ -23,9 +23,21 @@ module.exports = {
         "changeOrigin": true,
         "logLevel": "debug"
     },
-    "/alfresco": {
-        "target": "https://sse.dev.alfrescocloud.com",
-        "secure": false,
-        "changeOrigin": true
-      }
+    '/alfresco': {
+        target: "https://sse.dev.alfrescocloud.com",
+        secure: false,
+        logLevel: 'debug',
+        changeOrigin: true,
+        onProxyReq: function(request) {
+            if(request["method"] !== "GET")
+            request.setHeader("origin", host);
+        },
+        // workaround for REPO-2260
+        onProxyRes: function (proxyRes, req, res) {
+            const header = proxyRes.headers['www-authenticate'];
+            if (header && header.startsWith('Basic')) {
+                proxyRes.headers['www-authenticate'] = 'x' + header;
+            }
+        },
+    }
 }
