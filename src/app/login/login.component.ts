03bc390ce477a9084503b38d6c4c313a16cd3d2f
Rob Wilds <Robert.wilds@hyland.com>
2023-08-08 14:08:02 -0400

    added notifications

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,4 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
+
+  setDefaultTheme(){
+    var theme = '{"primary":"#2196f3","accent":"#ff9800","name":"ECM Blue Orange","href":"adf-blue-orange.css","isDark":false}';
+    this.storageService.setItem("docs-theme-storage-current", theme);
+    this.styleManager.setStyle('theme',JSON.parse(theme).href);
+    console.log('*** THEME SET ***');
+}
 }
