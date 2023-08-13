0fc0bfbce559c8c68195e062c487e608a8d5a7e9
Rob Wilds <robwilds@me.com>
2023-08-13 16:12:49 -0400

    viewer panel working in docs but needs work

--- src/app/app-layout/app-layout.component.ts
+++ src/app/app-layout/app-layout.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslationService, AuthenticationService } from '@alfresco/adf-core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app-layout.component.html',
   styleUrls: ['./app-layout.component.scss']
 })
-export class AppLayoutComponent {}
+export class AppLayoutComponent implements OnInit{
+
+  userName: string = null;
+
+constructor (private authService: AuthenticationService) {
+
+
+}
+
+ngOnInit(): void {
+    
+  this.userName = this.authService.getEcmUsername();
+}
+}
