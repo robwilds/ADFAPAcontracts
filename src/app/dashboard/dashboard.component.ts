34f321df247189e65cf6f79c3b74bf1ec3717156
Rob Wilds <robwilds@me.com>
2023-08-14 10:23:11 -0400

    minor updates

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -26,16 +26,16 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
         transition(
           ':enter', 
           [
-            style({ height: 0, opacity: 0 }),
-            animate('1s ease-out', 
+            style({ height: 0, opacity: 1 }),
+            animate('2s ease-out', 
                     style({ height: 300, opacity: 1 }))
           ]
         ),
         transition(
           ':leave', 
           [
-            style({ height: 300, opacity: 1 }),
-            animate('1s ease-in', 
+            style({ height: 0, opacity: 1 }),
+            animate('2s ease-in', 
                     style({ height: 0, opacity: 0 }))
           ]
         )
