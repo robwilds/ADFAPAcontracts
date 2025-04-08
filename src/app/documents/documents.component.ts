82cc6231bfeb12342ff1679c102faaa8c4474fbf
Rob Wilds <robwilds@me.com>
2025-04-08 16:45:52 -0400

    add link but need to add animation to button and popup

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,67 +1,78 @@
-import { Component, ViewChild, Input, OnInit } from '@angular/core';
-import { NotificationService,DisplayMode,AppConfigService,AppConfigValues } from '@alfresco/adf-core';
-import { DocumentListComponent, NodeEntityEvent, NodeEntryEvent,ContentService,
+import { Component, ViewChild, Input, OnInit } from "@angular/core";
+import {
+  NotificationService,
+  DisplayMode,
+  AppConfigService,
+  AppConfigValues,
+} from "@alfresco/adf-core";
+import {
+  DocumentListComponent,
+  NodeEntityEvent,
+  NodeEntryEvent,
+  ContentService,
   FolderCreatedEvent,
-  UploadService,PermissionStyleModel,
+  UploadService,
+  PermissionStyleModel,
   UploadFilesEvent,
   ConfirmDialogComponent,
   ContentMetadataService,
   FilterSearch,
   DialogAspectListService,
-  FileUploadEvent, } from '@alfresco/adf-content-services';
-import { PreviewService } from '../services/preview.service';
-import { ActivatedRoute,Router,PRIMARY_OUTLET } from '@angular/router';
-import { MinimalNode, MinimalNodeEntity,NodeEntry,SiteEntry,Node} from '@alfresco/js-api';
-import { NodesApiService } from '@alfresco/adf-content-services';
-import { trigger, state, style, animate, transition } from '@angular/animations';
-import { empty } from '@apollo/client';
-import { AssociationsComponent } from 'app/associations/associations.component';
-import { MatDialog } from '@angular/material/dialog';
-import { MetadataDialogAdapterComponent } from './metadata-dialog-adapter.component';
-import { interval } from 'rxjs';
-import { MatSnackBar } from '@angular/material/snack-bar';
+  FileUploadEvent,
+} from "@alfresco/adf-content-services";
+import { PreviewService } from "../services/preview.service";
+import { ActivatedRoute, Router, PRIMARY_OUTLET } from "@angular/router";
+import {
+  MinimalNode,
+  MinimalNodeEntity,
+  NodeEntry,
+  SiteEntry,
+  Node,
+} from "@alfresco/js-api";
+import { NodesApiService } from "@alfresco/adf-content-services";
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from "@angular/animations";
+import { empty } from "@apollo/client";
+import { AssociationsComponent } from "app/associations/associations.component";
+import { MatDialog } from "@angular/material/dialog";
+import { MetadataDialogAdapterComponent } from "./metadata-dialog-adapter.component";
+import { interval } from "rxjs";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
-  selector: 'app-documents',
-  templateUrl: './documents.component.html',
-  styleUrls: ['./documents.component.scss'],
+  selector: "app-documents",
+  templateUrl: "./documents.component.html",
+  styleUrls: ["./documents.component.scss"],
   animations: [
-    trigger(
-      'inOutAnimation',
-      [
-        transition(
-          ':enter',
-          [
-            style({ height: 0, opacity: 0 }),
-            animate('3s ease-out',
-                    style({ height: 300, opacity: 1 }))
-          ]
-        ),
-        transition(
-          ':leave',
-          [
-            style({ height: 300, opacity: 1 }),
-            animate('3s ease-in',
-                    style({ height: 0, opacity: 0 }))
-          ]
-        )
-      ]
-    )
-  ]
+    trigger("inOutAnimation", [
+      transition(":enter", [
+        style({ height: 0, opacity: 0 }),
+        animate("3s ease-out", style({ height: 300, opacity: 1 })),
+      ]),
+      transition(":leave", [
+        style({ height: 300, opacity: 1 }),
+        animate("3s ease-in", style({ height: 0, opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
-export class DocumentsComponent implements OnInit{
-
-  @ViewChild('documentList', { static: true })
+export class DocumentsComponent implements OnInit {
+  @ViewChild("documentList", { static: true })
   documentList: DocumentListComponent;
   fxFlexForDocList: number = 0;
   fxFlexForAux: number = 0;
   snackBarDuration = 3000;
 
-  isHidden:boolean = false;
+  isHidden: boolean = false;
 
-  showMetaContent:boolean = false;
-  showDocListContent: boolean=true;
-  showPreviewContent: boolean=false;
+  showMetaContent: boolean = false;
+  showDocListContent: boolean = true;
+  showPreviewContent: boolean = false;
 
   @Input()
   showViewer: boolean = false;
@@ -80,28 +91,39 @@ export class DocumentsComponent implements OnInit{
   nodeEntry: any;
   sub: any;
 
-  folderNodeAssociation:string;
-  showAssocations:boolean = false;
+  folderNodeAssociation: string;
+  showAssocations: boolean = false;
 
   displayMode = DisplayMode.List;
-  displayEmptyMetadata:boolean = true;
+  displayEmptyMetadata: boolean = true;
 
-  baseShareUrl = (
-    this.appConfig.get<string>(AppConfigValues.BASESHAREURL) ||
-    this.appConfig.get<string>(AppConfigValues.ECMHOST)) + '/preview/s/';
+  baseShareUrl =
+    (this.appConfig.get<string>(AppConfigValues.BASESHAREURL) ||
+      this.appConfig.get<string>(AppConfigValues.ECMHOST)) + "/preview/s/";
 
-  constructor(private appConfig: AppConfigService,private _snackBar: MatSnackBar, private dialog: MatDialog,private dialogAspectListService: DialogAspectListService,private contentService: ContentService,private route: ActivatedRoute, public router: Router, private notificationService: NotificationService, private preview: PreviewService, private nodeService: NodesApiService) {
-  this.fxFlexForDocList = 100;
-  this.fxFlexForAux = 0;
+  constructor(
+    private appConfig: AppConfigService,
+    private _snackBar: MatSnackBar,
+    private dialog: MatDialog,
+    private dialogAspectListService: DialogAspectListService,
+    private contentService: ContentService,
+    private route: ActivatedRoute,
+    public router: Router,
+    private notificationService: NotificationService,
+    private preview: PreviewService,
+    private nodeService: NodesApiService
+  ) {
+    this.fxFlexForDocList = 100;
+    this.fxFlexForAux = 0;
   }
 
   ngOnInit(): void {
-    this.sub = this.route
-    .params
-    .subscribe
-    (
-      value => (this.currentFolderId = value['fid'] == null? '5be4a4cc-f413-4f28-8329-dce29671b224' : value['fid'])
-
+    this.sub = this.route.params.subscribe(
+      (value) =>
+        (this.currentFolderId =
+          value["fid"] == null
+            ? "5be4a4cc-f413-4f28-8329-dce29671b224"
+            : value["fid"])
     );
   }
   onViewerVisibilityChanged() {
@@ -112,7 +134,7 @@ export class DocumentsComponent implements OnInit{
   }
 
   uploadSuccess(event: any) {
-    this.notificationService.openSnackMessage('File uploaded');
+    this.notificationService.openSnackMessage("File uploaded");
     this.documentList.reload();
   }
 
@@ -128,187 +150,204 @@ export class DocumentsComponent implements OnInit{
     this.nodeId = null;
   }
 
-  onSearchSubmit(event:any){
-    console.log("on search submit: ", event)
+  onSearchSubmit(event: any) {
+    console.log("on search submit: ", event);
   }
 
   onSearchItemClicked(event: MinimalNodeEntity) {
-
-    console.log("option clicked object: ", event.entry.id)
-    console.log("seach item object: ",event)
+    console.log("option clicked object: ", event.entry.id);
+    console.log("seach item object: ", event);
     if (event.entry.isFile === true) {
       this.currentFolderId = event.entry.parentId;
-
     } else {
       this.currentFolderId = event.entry.id;
     }
-}
-
-nodeClicked(event: NodeEntityEvent){
-
-  if (this.documentList.selection[0].entry.isFile === true) {
-
-  this.fxFlexForDocList = 0;
-  this.isHidden = true;
-  this.showDocListContent = false;
-  this.fxFlexForAux = 50;
-
-  this.nodeId = this.documentList.selection[0].entry.id;
-  this.node = this.documentList.selection[0].entry;
-
-  console.log("clicked node id:", this.nodeId);
-  console.log("document list object for clicked node is: ",this.node);
-
-  this.showViewer = false;
-  this.showViewer = true;
-  this.showView = true;
   }
-  else{
-    this.isHidden = false;
+
+  nodeClicked(event: NodeEntityEvent) {
+    if (this.documentList.selection[0].entry.isFile === true) {
+      this.fxFlexForDocList = 0;
+      this.isHidden = true;
+      this.showDocListContent = false;
+      this.fxFlexForAux = 50;
+
+      this.nodeId = this.documentList.selection[0].entry.id;
+      this.node = this.documentList.selection[0].entry;
+
+      console.log("clicked node id:", this.nodeId);
+      console.log("document list object for clicked node is: ", this.node);
+
+      this.showViewer = false;
+      this.showViewer = true;
+      this.showView = true;
+    } else {
+      this.isHidden = false;
+      this.showViewer = false;
+      this.fxFlexForDocList = 100;
+      this.fxFlexForAux = 0;
+      this.showDocListContent = true;
+      //this.showMetaContent = false;
+      this.nodeId = this.documentList.selection[0].entry.id;
+      this.node = this.documentList.selection[0].entry;
+
+      //console.log("clicked foldernode id:", this.nodeId);
+      console.log(
+        "document list object for clicked foldernode is: ",
+        this.node.properties["ContractManagement:associations"]
+      );
+    }
+  }
+
+  onAspectUpdate(event: any) {
+    this.dialogAspectListService
+      .openAspectListDialog(event.value.entry.id)
+      .subscribe((aspectList) => {
+        this.nodeService
+          .updateNode(event.value.entry.id, { aspectNames: [...aspectList] })
+          .subscribe(() => {
+            this.openSnackMessageInfo("Node Aspects Updated");
+          });
+      });
+  }
+
+  backButtonClicked() {
+    this.fxFlexForDocList = 100;
+    this.fxFlexForAux = 0;
+    this.showDocListContent = true;
+  }
+
+  viewerClosed() {
     this.showViewer = false;
     this.fxFlexForDocList = 100;
     this.fxFlexForAux = 0;
     this.showDocListContent = true;
-    //this.showMetaContent = false;
-    this.nodeId = this.documentList.selection[0].entry.id;
-    this.node = this.documentList.selection[0].entry;
-
-  //console.log("clicked foldernode id:", this.nodeId);
-  console.log("document list object for clicked foldernode is: ",this.node.properties['ContractManagement:associations']);
-
-  }
-}
-
-onAspectUpdate(event: any) {
-  this.dialogAspectListService.openAspectListDialog(event.value.entry.id).subscribe((aspectList) => {
-      this.nodeService.updateNode(event.value.entry.id, { aspectNames: [...aspectList] }).subscribe(() => {
-          this.openSnackMessageInfo('Node Aspects Updated');
-      });
-  });
-}
-
-backButtonClicked(){
-  this.fxFlexForDocList = 100;
-    this.fxFlexForAux = 0;
-    this.showDocListContent = true;
-
-}
-
-viewerClosed(){
-  this.showViewer = false;
-  this.fxFlexForDocList = 100;
-    this.fxFlexForAux = 0;
-    this.showDocListContent = true;
     this.isHidden = false;
-}
+  }
 
-loginfo(inf:any){
-  console.log("info from associations is: ",inf);
-}
+  loginfo(inf: any) {
+    console.log("info from associations is: ", inf);
+  }
 
-folderAssociationsCustomAction(event) {
-  //let entry = event.value.entry;
+  folderAssociationsCustomAction(event) {
+    //let entry = event.value.entry;
 
-  this.folderNodeAssociation = event.value.entry;
-  this. showAssocations = true;
-console.log("association clicked", this.folderNodeAssociation,"show state ",this.showAssocations)
-  //alert(`Custom document action for ${entry.id}`);
-}
+    this.folderNodeAssociation = event.value.entry;
+    this.showAssocations = true;
+    console.log(
+      "association clicked",
+      this.folderNodeAssociation,
+      "show state ",
+      this.showAssocations
+    );
+    //alert(`Custom document action for ${entry.id}`);
+  }
 
-closeAssociation(){
-  this.showAssocations = false;
-}
+  closeAssociation() {
+    this.showAssocations = false;
+  }
 
-toggleGalleryView(): void {
-  this.displayMode = this.displayMode === DisplayMode.List ? DisplayMode.Gallery : DisplayMode.List;
-  const url = this.router.createUrlTree(['/files', this.currentFolderId, 'display', this.displayMode]).toString();
+  toggleGalleryView(): void {
+    this.displayMode =
+      this.displayMode === DisplayMode.List
+        ? DisplayMode.Gallery
+        : DisplayMode.List;
+    const url = this.router
+      .createUrlTree([
+        "/files",
+        this.currentFolderId,
+        "display",
+        this.displayMode,
+      ])
+      .toString();
 
-  //this.location.go(url);
-}
+    //this.location.go(url);
+  }
 
-openSnackMessageError(error: any) {
-  this.notificationService.showError(error.value || error);
-}
+  openSnackMessageError(error: any) {
+    this.notificationService.showError(error.value || error);
+  }
 
-openSnackMessageInfo(message: string) {
-  this.notificationService.showInfo(message);
-}
+  openSnackMessageInfo(message: string) {
+    this.notificationService.showInfo(message);
+  }
 
-canEditFolder(selection: Array<NodeEntry>): boolean {
-  if (selection && selection.length === 1) {
+  canEditFolder(selection: Array<NodeEntry>): boolean {
+    if (selection && selection.length === 1) {
       const entry = selection[0].entry;
 
       if (entry && entry.isFolder) {
-          return this.contentService.hasAllowableOperations(entry, 'update');
+        return this.contentService.hasAllowableOperations(entry, "update");
       }
+    }
+    return false;
   }
-  return false;
-}
 
-onDeleteActionSuccess(message: string) {
-  //this.uploadService.fileDeleted.next(message);
-  //this.deleteElementSuccess.emit();
-  this.documentList.reload();
-  this.openSnackMessageInfo(message);
-}
-onManageMetadata(event: any) {
-  const contentEntry = event.value.entry;
-  const displayEmptyMetadata = this.displayEmptyMetadata;
+  onDeleteActionSuccess(message: string) {
+    //this.uploadService.fileDeleted.next(message);
+    //this.deleteElementSuccess.emit();
+    this.documentList.reload();
+    this.openSnackMessageInfo(message);
+  }
+  onManageMetadata(event: any) {
+    const contentEntry = event.value.entry;
+    const displayEmptyMetadata = this.displayEmptyMetadata;
 
-  if (this.contentService.hasAllowableOperations(contentEntry, 'update')) {
+    if (this.contentService.hasAllowableOperations(contentEntry, "update")) {
       this.dialog.open(MetadataDialogAdapterComponent, {
-          data: {
-              contentEntry,
-              displayEmptyMetadata
-          },
-          panelClass: 'adf-metadata-manager-dialog',
-          width: '630px'
+        data: {
+          contentEntry,
+          displayEmptyMetadata,
+        },
+        panelClass: "adf-metadata-manager-dialog",
+        width: "630px",
       });
-  } else {
-      this.openSnackMessageError('OPERATION.ERROR.PERMISSION');
+    } else {
+      this.openSnackMessageError("OPERATION.ERROR.PERMISSION");
+    }
   }
-}
 
-openSnackBar(message: string, action: string) {
-  this._snackBar.open(message, action, {
-    duration: this.snackBarDuration
-  });
-}
-
-onSiteChange(site: SiteEntry) {
-  this.currentFolderId = site.entry.guid;
-}
-
-backToContracts(){
-  //go back to default folder
-  this.refreshDocList("Back to Contracts!");
-
-  console.log("Going back to contracts: ",this.documentList.currentFolderId)
-}
-
-onMyFilesDragDropSuccess(event: Object): void {
-  console.log('File uploaded');
-  this.refreshDocList();
-}
-
-refreshDocList(message?:string){
-
-  this.currentFolderId = "nothing";
-              interval(10).subscribe(val => { this.currentFolderId = "5be4a4cc-f413-4f28-8329-dce29671b224"})
-
-              if (message){this.openSnackBar(message,"");}
-
-}
-
-canCreateContent(parentNode: Node): boolean {
-  if (parentNode) {
-      return this.contentService.hasAllowableOperations(parentNode, 'create');
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: this.snackBarDuration,
+    });
   }
-  return false;
-}
 
-hasSelection(selection: Array<any>): boolean {
-  return selection && selection.length > 0;
-}
+  onSiteChange(site: SiteEntry) {
+    this.currentFolderId = site.entry.guid;
+  }
 
+  backToContracts() {
+    //go back to default folder
+    this.refreshDocList("Back to Contracts!");
+
+    console.log("Going back to contracts: ", this.documentList.currentFolderId);
+  }
+
+  aiSearch() {}
+  onMyFilesDragDropSuccess(event: Object): void {
+    console.log("File uploaded");
+    this.refreshDocList();
+  }
+
+  refreshDocList(message?: string) {
+    this.currentFolderId = "nothing";
+    interval(10).subscribe((val) => {
+      this.currentFolderId = "5be4a4cc-f413-4f28-8329-dce29671b224";
+    });
+
+    if (message) {
+      this.openSnackBar(message, "");
+    }
+  }
+
+  canCreateContent(parentNode: Node): boolean {
+    if (parentNode) {
+      return this.contentService.hasAllowableOperations(parentNode, "create");
+    }
+    return false;
+  }
+
+  hasSelection(selection: Array<any>): boolean {
+    return selection && selection.length > 0;
+  }
 }
