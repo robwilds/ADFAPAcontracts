a08a9678af635025137ba1657d64bafca5de7448
Rob Wilds <robwilds@me.com>
2025-04-09 15:53:10 -0400

    added chatbot

--- src/app/ai-search/ai-search.component.ts
+++ src/app/ai-search/ai-search.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Message, ChatService } from "./chat.service";
 
 @Component({
-  selector: 'ai-search',
-  templateUrl: './ai-search.component.html',
-  styleUrls: ['./ai-search.component.css']
+  selector: "ai-search",
+  templateUrl: "./ai-search.component.html",
+  styleUrls: ["./ai-search.component.css"],
+  providers: [ChatService],
 })
 export class AiSearchComponent {
+  messages: Message[] = [];
+  value: string;
 
+  constructor(public chatService: ChatService) {}
+
+  ngOnInit() {
+    this.chatService.conversation.subscribe((val) => {
+      this.messages = this.messages.concat(val);
+    });
+  }
+
+  sendMessage() {
+    this.chatService.getBotAnswer(this.value);
+    this.value = "";
+  }
 }
---
+++ src/app/ai-search/chat.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from "@angular/core";
+import { Observable, Subject } from "rxjs";
+
+export class Message {
+  constructor(public author: string, public content: string) {}
+}
+
+@Injectable()
+export class ChatService {
+  audioFile = new Audio(
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/success.mp3"
+  );
+  constructor() {}
+
+  conversation = new Subject<Message[]>();
+
+  messageMap = {
+    Hi: "Hello",
+    "Who are you": "My name is Angular Bot",
+    "What is Angular": "Angular is the best framework ever",
+    default: "I can't understand. Can you please repeat",
+  };
+
+  getBotAnswer(msg: string) {
+    const userMessage = new Message("user", msg);
+    this.conversation.next([userMessage]);
+    const botMessage = new Message("bot", this.getBotMessage(msg));
+
+    setTimeout(() => {
+      this.playFile("nothing yet");
+      this.conversation.next([botMessage]);
+    }, 1500);
+  }
+
+  playFile(sound: string) {
+    this.audioFile.play();
+  }
+
+  playAudio() {
+    this.playFile("https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/success.mp3");
+  }
+
+  getBotMessage(question: string) {
+    let answer = this.messageMap[question];
+    return answer || this.messageMap["default"];
+  }
+}
